949f16718d0eb427b6a628f5605725cc029304ad
Hisham Abdul Hai <hishamabdulhai@proton.me>
2022-07-11 23:22:49 -0300

    transcode h264 clips to dnxhd file that are recognized by davinci resolve using ffmpeg

---
+++ .local/bin/dnxhd.sh
@@ -0,0 +1,24 @@
+#/bin/sh
+
+# Usage: dnxhd.sh directory
+#
+#
+# First we change directory to where the files we want to transcode are located
+cd $1
+
+# A "for" loop is used to loop through all the raw clips
+for i in *.MOV
+do
+    # To get the file name without extension, the basename command can be used
+    output=$(basename -s .MOV $i)
+
+    # With ffmpeg command we transcode the clip to dnxhd codec
+    # dnxhd is the desired video codec
+    # fps=30000/1001=29.97 is the framerate of Nikon D7100 raw clips
+    # pcm_s16le is the audio codec supported by Davinci Resolve Free
+    # yuv422p10 is the pixel format
+    # The new file name will be the original file name but with a different extension
+    ffmpeg -hide_banner -i $i -c:v dnxhd -vf "scale=1920:1080,fps=30000/1001,format=yuv422p10" -b:v 175M -c:a pcm_s16le $output.mov
+
+# The "for" loop is done
+done
