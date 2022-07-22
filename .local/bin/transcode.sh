eac093d3d922c9980f9ced115ee7fe19b53337fd
Hisham Abdul Hai <hishamabdulhai@proton.me>
2022-07-21 22:07:38 -0300

    replacing basename command in the first block with a more elegant solution, and adding an input argument which allow the user to enter a filename

--- .local/bin/transcode.sh
+++ .local/bin/transcode.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
-# Usage: transcode.sh file or directory
 #
+# Usage: transcode.sh $1 $2
+# $1 Could be a file or a directory
+# $2 Could be empty or a string to be used instead of the orignal file name (applicable only when the first input argument is a file)
 #
 # If the argument is a directory, then, we want to transcode the files with specific format inside this directory to dnxhd codec
 if [[ -d $1 ]]; then
@@ -9,31 +10,28 @@ if [[ -d $1 ]]; then
     # A "for" loop is used to loop through all the raw clips
     for i in {*.MOV,*.mkv}
     do
-	# First let's check the file extension to determine which one is MOV and which one is mkv.
-	if [[ $i == *.MOV ]]; then
-	    # To get the file name without extension, the basename command can be used
-	    output=$(basename -s .MOV $i)
-	else
-	    output=$(basename -s .mkv $i)
-	fi
-
+	# Get the filename without extension.
+	output=${i%.*}
+	#
 	# With ffmpeg command we transcode the clip to dnxhd codec
 	# dnxhd is the desired video codec
         # fps=30000/1001=29.97 is the framerate of Nikon D7100 raw clips
         # pcm_s16le is the audio codec supported by Davinci Resolve Free
         # yuv422p10 is the pixel format
         # The new file name will be the original file name but with a different extension
-	ffmpeg -hide_banner -i $i -c:v dnxhd -vf "scale=1920:1080,fps=30000/1001,format=yuv422p10" -b:v 175 -c:a pcm_s16le $1/$output.mov
-
+	ffmpeg -hide_banner -i $i -c:v dnxhd -vf "scale=1920:1080,fps=30000/1001,format=yuv422p10" -b:v 175M -c:a pcm_s16le $output.mov
+	#
 	# The "for" loop is done
     done
-
+    #
     # If the argument is a file, then, we want to transcode the file to h265 codec
 elif [[ -f $1 ]]; then
-    # change directory to where the file is located, for organization porposes
-    cd $(dirname $1)
-    # Get the filename without extension
-    file=$(basename -s .mov $1)
-    # Do the transcoding process
-    ffmpeg -hide_banner -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -i $1 -c:a aac -c:v hevc_nvenc -b:v 5M $file.mp4
+    # If a specific name is given in the second input argument, use it.
+    if [ -n "$2" ]; then
+	file=$2
+    else
+    # Else, use the name of the original file and change the extension
+	file=$(basename -s .mov $1)
+    fi
+    ffmpeg -hide_banner -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -i $1 -c:a aac -c:v hevc_nvenc -b:v 5M $(dirname $1)/$file.mp4
 fi
