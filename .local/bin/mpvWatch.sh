c06658c4a694e61be711b1234a398159dfad4c5f
Hisham <hishamabdulhai@proton.me>
2023-04-06 09:47:48 -0300

    use find command instead of ls

--- .local/bin/mpvWatch.sh
+++ .local/bin/mpvWatch.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-ls --sort=time /mnt/MISC | dmenu -p "File " -i -x 0 -y 0 -z 1700 -g 3 -l 30 | xargs -I {} mpv --cache=yes "/mnt/MISC/{}"
+if [ $1 == "list" ]; then
+    #ls --sort=time /mnt/videos | dmenu -p "File " -i -x 0 -y 0 -z 1700 -g 3 -l 30 | xargs -I {} mpv --cache=yes "/mnt/videos/{}"
+    find /mnt/videos -type f -printf "%T@ %f\n" | sort -r | cut -d' ' -f2 | dmenu -p "File " -i -x 0 -y 0 -z 1700 -g 3 -l 30 | xargs -I {} mpv --cache=yes "/mnt/videos/{}"
+elif [ $1 == "play" ]; then
+    find /mnt/videos -type f -exec echo {} >> /tmp/playlist.txt \; && mpv --shuffle --cache=yes --playlist=/tmp/playlist.txt && rm /tmp/playlist.txt
+fi
