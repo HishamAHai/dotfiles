5222a52f0a51a404a35a7b2a7d00b2a15e8eceda
Hisham <hishamabdulhai@proton.me>
2023-04-12 11:36:29 -0300

    updated to version 5.2

--- .config/dmenu/config.def.h
+++ .config/dmenu/config.def.h
@@ -2,19 +2,32 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xc8;     /* Amount of opacity. 0xff is opaque             */
+static const int user_bh = 8;               /* add an defined amount of pixels to the bar height */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"FantasqueSansMono Nerd Font:size=12",
+    "Noto Color Emoji:size=11:antialias=true:hinting=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+
+#include "Material_Dark.h" /* ColorScheme to be used */
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeSelHighlight] = { OPAQUE, alpha },
+	[SchemeNormHighlight] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+	[SchemeHp]  = { OPAQUE, alpha }
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 42;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
