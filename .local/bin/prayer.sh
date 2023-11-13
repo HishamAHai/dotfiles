3e6bdc03e237d5117089bd9bc253509e89be7651
Hisham <hishamabdulhai@proton.me>
2023-11-13 19:55:23 -0300

    add hijri date

--- .local/bin/prayer.sh
+++ .local/bin/prayer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 prayers="/mnt/nasbkup/prayers.json"
-currenttime=$(date +%s)  # Use timestamp instead of explicit time
+currenttime=$(date +%s)  # Use timestamp instead of HH:MM format
 
 # Function to calculate remaining time
 calculate_remain() {
@@ -22,6 +22,11 @@ get_prayer_time() {
     esac
 }
 
+#Hijri date
+day=$(jq ".data.date.hijri.day" $prayers | bc | awk '{$1=$1};1')
+month=$(jq ".data.date.hijri.month.ar" $prayers | bc | awk '{$1=$1};1')
+year=$(jq ".data.date.hijri.year" $prayers | bc | awk '{$1=$1};1')
+
 # Array of prayers
 prayer_names=("Fajr" "Sunrise" "Dhuhr" "Asr" "Maghrib" "Isha")
 
@@ -36,11 +41,11 @@ for i in {0..5}; do
     next_prayer_time=$(jq ".data.timings.$next_prayer" $prayers | bc | awk '{$1=$1};1')
     next_prayer_timestamp=$(date -d "$next_prayer_time" +%s)
 
-    if [[ $currenttime -ge $current_prayer_timestamp ]] && [[ $currenttime -lt $next_prayer_timestamp ]]; then
+    if [[ $currenttime -gt $current_prayer_timestamp ]] && [[ $currenttime -lt $next_prayer_timestamp ]]; then
         break
     fi
 done
 
 remain=$(calculate_remain $next_prayer_timestamp)
 
-printf '{"text": "%s"}\n' "🕌 الصلاة القادمة ۩ $(get_prayer_time $next_prayer) ۩ $next_prayer_time (الوقت المتبقي $remain)"
+printf '{"text": "%s"}\n' "🕌 $day $month $year ☪️الصلاة القادمة ۩ $(get_prayer_time $next_prayer) ۩ $next_prayer_time ☪️ الوقت المتبقي $remain 🕌"
